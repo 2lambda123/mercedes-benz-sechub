86f4e7cd565bb9930e770682f6592694502e7a9c
Restyled.io <commits@restyled.io>
2023-12-29 13:57:49 +0000

    Restyled by shfmt

--- sechub-pds-solutions/xray/tests/compress.sh
+++ sechub-pds-solutions/xray/tests/compress.sh
@@ -3,21 +3,16 @@
 
 current_test_folder="$1"
 test_file="$2"
-	
-if [[ ! -d "$current_test_folder" ]]
-then
-	echo "Target folder is empty"
-	exit 1
+
+if [[ ! -d "$current_test_folder" ]]; then
+  echo "Target folder is empty"
+  exit 1
 fi
 
-if [[ -z "$test_file" ]]
-then
-	echo "No test file provided"
-	exit 1
+if [[ -z "$test_file" ]]; then
+  echo "No test file provided"
+  exit 1
 fi
 cp -r "$test_file" "$current_test_folder/$test_file"
 cd "$current_test_folder" || exit
 tar -cvf "$test_file".tar "$test_file"
-
-
-
